eeb57f9f13234b995ac93cccdcb3af2191837b92
landryx <landry@lnts.me>
2024-11-11 21:50:33 +0100

    A bit of data engineering with Java

--- demo-file-reading/src/main/java/com/test/Main.java
+++ demo-file-reading/src/main/java/com/test/Main.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,6 +10,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
+import java.math.BigInteger;
 
 public class Main {
 
@@ -62,10 +66,60 @@ public class Main {
 
     }
 
+    public static void getGreaterThan9999(String filename) {
+
+        try{
+            String[] nums = readFileAsString(filename).split(" ");
+            int counter = 0;
+            
+            for (int i = 0; i < nums.length; i++) {
+                if (Integer.parseInt(nums[i]) >= 9999) {
+                    counter+=1;
+                }
+            }
+            System.out.println(counter);
+
+        }catch(FileNotFoundException e) {
+            System.out.println("There is no file on the specified location: " + filename);
+        } catch (IOException e) {
+            System.out.println("There is no file on the specified location: " + filename);
+        }
+
+    }
+
+    public static void largestIncrease(String filename) {
+        
+        try (Scanner scan = new Scanner(new File(filename))) {
+            Double dif = 0d;    
+            String year = "";
+            String[] line;
+            String[] prevLine = {};
+            scan.nextLine();
+            while(scan.hasNext()) {
+                line = scan.nextLine().split("\t");
+                if (prevLine.length == 0) {
+                    prevLine = line;
+                } else {
+                    if (Double.parseDouble(line[1].replaceAll(",", "")) - Double.parseDouble(prevLine[1].replaceAll(",", "")) > dif) {
+                        dif = Double.parseDouble(line[1].replaceAll(",", "")) - Double.parseDouble(prevLine[1].replaceAll(",", ""));
+                        year = line[0];
+                    } 
+                    prevLine = line;
+                }
+            }
+            System.out.println(year);
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("There is no file on the specified location: " + filename);
+        }
+    }
+
     public static void main(String[] args) {
         String pathToFile = "/home/drux/Java/project/demo-file-reading/data/";
         File file = new File(pathToFile + "dataset_91033.txt");
         String numbersPath = pathToFile + "dataset_91007.txt";
+        String numbers2Path = pathToFile + "dataset_91022.txt";
+        String populationFilePath = pathToFile + "dataset_91069.txt";
         //var scanner = new Scanner(new URL("http://www.google.com").openStream(), "UTF-8");
 
         try(Scanner scanner = new Scanner(file)) {
@@ -81,10 +135,13 @@ public class Main {
 
         //Get the greater number
         //in the file
-        getGreater(numbersPath);
+        //getGreater(numbersPath);
 
         //get only the even numbers
-        String url = "https://stepik.org/media/attachments/lesson/91065/dataset_91065.txt";
-        getEven(url);
+        //String url = "https://stepik.org/media/attachments/lesson/91065/dataset_91065.txt";
+        //getEven(url);
+
+        //getGreaterThan9999(numbers2Path);
+        largestIncrease(populationFilePath);
     }
 }
