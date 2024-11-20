94cd000d1db2804ec38b6180662fbc7cd773e09c
landryx <landry@lnts.me>
2024-11-20 21:37:18 +0100

    testing template pattern

---
+++ demo-file-reading/src/main/java/com/templatePatern/Programmer.java
@@ -0,0 +1,12 @@
+package com.templatePatern;
+
+public class Programmer extends Worker {
+    
+    @Override
+    public void workingProcess () {
+        System.out.println(" > I'm a programmer");
+        System.out.println(" > I drink coffee");
+        System.out.println(" > I write code");
+        System.out.println(" > I drink coffe again");
+    }
+}
--- demo-file-reading/src/main/java/com/test/Main.java
+++ demo-file-reading/src/main/java/com/test/Main.java
@@ -1,23 +1,15 @@
 package com.test;
 
-import com.core.MessageSender;
-import com.startegypattern.EmailSendingMethod;
-import com.startegypattern.SmsSendingMethod;
+import com.templatePatern.Programmer;
+import com.templatePatern.Worker;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        MessageSender sender = new MessageSender(); // create a message sender
-
-        sender.setMethod(new EmailSendingMethod()); // set a concrete sending method
-
-        sender.send("alice@gmail.com", "bob@gmail.com", "Hello!");
-
-        sender.setMethod(new SmsSendingMethod()); // set another sending method
-
-        sender.send("1-541-444-3333", "1-541-555-2222", "Hello!");
+        Worker programmer = new Programmer();
+        programmer.work();
             
     }
 }
