10e5f501e1f263bf7c892e0f7fd19f045be668bb
landryx <landry@lnts.me>
2025-01-03 23:44:03 +0100

    unit tests in java - hyperskill learning

--- demo-file-reading/src/test/java/com/commons/UserTest.java
+++ demo-file-reading/src/test/java/com/commons/UserTest.java
@@ -5,30 +5,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
 
-    @Test
-    void hasStrongPassword() {
+    /*
+    User user;
+
+    @BeforeEach
+    void createUser() {
         String username = "Alice";
         String password = "12345678";
-        User user = new User(username, password);
+        user = new User(username, password);
+    }
 
+    @Test
+    void hasStrongPassword() {
+        System.out.println("Testing for strong password");
         assertTrue(user.hasStrongPassword());
     }
 
     @Test
     void hasValidUsername() {
-        String username = "Alice";
-        String password = "12345678";
-        User user = new User(username, password);
-
+        System.out.println("Testing for valid username");
         assertTrue(user.hasValidUsername());
     }
 
     @Test
     void isValid() {
-        String username = "Alice";
-        String password = "12345678";
-        User user = new User(username, password);
-
+        System.out.println("Testing for valid user");
         assertTrue(user.isValid());
+    }
+    */
+
+    static int number = 14;
+
+    UserTest() {
+        number *= 2;
+    }
+
+    @BeforeAll
+    static void method3() {
+        number += 11;
+    }
+
+    @BeforeEach
+    void method2() {
+        number -= 4;
+    }
+
+    @AfterAll
+    static void method4() {
+        number /= 3;
+    }
+
+    @AfterEach
+    void method5() {
+        System.out.println(number);
+    }
+
+    @Test
+    void method6() {
+        number += 9;
     }
 }
