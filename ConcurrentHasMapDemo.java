7cd6f003936447eedbd1034d7527dc983ced455b
landryx <landry@lnts.me>
2025-05-19 22:13:01 +0200

    testing generic methods

--- ConcurrentHasMapDemo.java
+++ ConcurrentHasMapDemo.java
@@ -6,7 +6,7 @@ public class ConcurrentHasMapDemo {
     public static void main(String[] args) throws InterruptedException {
 
         Map<Integer, String> map = new ConcurrentHashMap<>();
-
+        Integer[] test = new Integer[]{1,2,3};
         Thread writer1 = new Thread(() -> addPositiveNumbers(map));
         Thread writer2 = new Thread(() -> addNegativeNumbers(map));
 
@@ -19,12 +19,18 @@ public class ConcurrentHasMapDemo {
         writer2.join(); // wait for writer2 thread
 
         System.out.println(map.size()); // the result is always the same
+        System.out.println(length(test));
+
     }
 
     private static void addPositiveNumbers(Map<Integer, String> target) {
         for (int i = 0; i < 100_000; i++) {
             target.put(i, "Number is " + i);
         }
+    }
+
+    public static <E> int length(E[] t) {
+        return t.length;
     }
 
     private static void addNegativeNumbers(Map<Integer, String> target) {
